31de2e21d6924bdbf7a8d6f2e69399b8851b6d77
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2025-09-12 12:35:15 -0300

    Added click outside effect

--- src/components/desserts/displays/DessertDetails.jsx
+++ src/components/desserts/displays/DessertDetails.jsx
@@ -1,4 +1,6 @@
+import { useRef } from "react";
 import { motion } from "motion/react";
+import { useOutsideClick } from "../../../effects/effects";
 import ListArticle from "../../articles/ListArticle";
 import DisplayBase from "../../displays/DisplayBase";
 import ReadOnlyHeading from "../../headings/ReadOnlyHeading";
@@ -8,9 +10,13 @@ import CloseButton from "../../buttons/CloseButton";
 import "./display-styles.css";
 
 
-function DessertDetails({dessert, onClose, onEdit}) {
+function DessertDetails({ dessert, onClose, onEdit }) {
 
     const steps = dessert.steps.split(". ").map((sentence) => sentence + ".");
+
+    const menuRef = useRef(null);
+
+    useOutsideClick(menuRef, () => onClose());
 
     return (
         <motion.div
@@ -18,22 +24,23 @@ function DessertDetails({dessert, onClose, onEdit}) {
             animate={{ opacity: 1, y: 0 }}
             exit={{ y: 200 }}
             transition={{ duration: 0.19, ease: "easeOut" }}
+            ref={menuRef}
         >
             <DisplayBase>
-                <div style={{display: "grid", gridTemplateColumns: "1fr auto", gridAutoFlow: "column"}}>
-                    <ReadOnlyHeading 
+                <div style={{ display: "grid", gridTemplateColumns: "1fr auto", gridAutoFlow: "column" }}>
+                    <ReadOnlyHeading
                         title1={"🍽️ " + dessert.name}
                         options={[
                             { name: "Cerrar", onClick: () => onClose(dessert) }
                         ]}
                     />
-                    <div style={{width: "45px", height: "45px"}}>
-                        <CloseButton onClick={onClose}/>
+                    <div style={{ width: "45px", height: "45px" }}>
+                        <CloseButton onClick={onClose} />
                     </div>
                 </div>
                 <div className="middle-row-container">
                     <InfoArticle title2="🛒 Ingredientes:" >
-                        <IngredientsList ingredients={dessert.ingredients}/>
+                        <IngredientsList ingredients={dessert.ingredients} />
                     </InfoArticle>
                     <div className="image-container">
                         <div className="image-border">
